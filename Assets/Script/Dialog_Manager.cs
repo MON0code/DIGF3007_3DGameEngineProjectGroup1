f692ae4286f2c286d55c3f92145e8d52a8dea8d4
1lyalee <Lizhaofeng1216@gmail.com>
2022-12-03 19:35:44 -0500

    Dialog Script added
    - Modified collision for some room object
    - Wrote dialog manager script and added component on chair
    - Link all objects in the script to be correctly referenced
    - Created text bubbles as sprite

---
+++ Assets/Script/Dialog_Manager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialog_Manager : MonoBehaviour
+{
+
+    // define gameobjects to manipulate
+
+    public GameObject Chair;
+
+    public GameObject begin_bubble; // begin dialog bubble
+
+    public GameObject All_bubbles; // all the other bubble
+
+    public GameObject Toilet;         // colliding objects
+    public GameObject Toilet_bubble;  // bubble show up for that specific object
+
+    public GameObject Basin;
+    public GameObject Basin_bubble;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        All_bubbles.SetActive(true);
+
+        if (collision.gameObject == Toilet)
+        {
+            begin_bubble.SetActive(false);
+
+            Toilet_bubble.SetActive(true);
+        }else if(collision.gameObject == Basin)
+        {
+            begin_bubble.SetActive(false);
+
+            Basin_bubble.SetActive(true);
+        }
+
+
+    }
+
+    // if not colliding, turn off all the bubbles 
+
+    void OnCollisionExit(Collision collision)
+    {
+        All_bubbles.SetActive(false);
+
+        for (int i = 0; i < All_bubbles.transform.childCount; i++)
+        {
+            var child = All_bubbles.transform.GetChild(i).gameObject;
+            child.SetActive(false);
+        }
+
+
+    }
+
+
+
+
+}
