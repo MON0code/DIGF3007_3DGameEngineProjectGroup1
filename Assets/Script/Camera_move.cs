5c7f723e26fda4592dec3a94dd66ec3f189d0014
1lyalee <Lizhaofeng1216@gmail.com>
2022-12-04 23:20:03 -0500

    Camera Movements according to Chair Movements

---
+++ Assets/Script/Camera_move.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_move : MonoBehaviour
+{
+    public Transform cameraaa;
+    public Transform chair;
+
+    void Update()
+    {   
+
+        // calculate the camera position based on the chair position
+        // values are designed to be a comfortable angle
+
+        float camera_x = chair.transform.position.x + 5.5f;
+        float camera_y = chair.transform.position.y + 3.5f;
+        float camera_z = chair.transform.position.y + -2.5f;
+
+        // transform the camera into the new position
+
+        cameraaa.transform.position = new Vector3(camera_x, camera_y, camera_z);
+
+    }
+
+
+}
